cc25b5c92a33a97e7d47a9d9f49b32bfd3a4f0e5
Mohan <mohan@hansaitechnosoft.com>
2023-05-03 13:56:24 +0530

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Diwakar Reddy  on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
